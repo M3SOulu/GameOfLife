11453e56a271992f07de697b36f9af5dc1f47516
somename <someemail>
2016-12-07 16:55:59 +0100

    besouro automatic message

--- src/classes/Grid.java
+++ src/classes/Grid.java
@@ -8,70 +8,71 @@ public class Grid {
 	private int height;
 	private Cell[][] grid;
 
-
-	public Grid(ArrayList<Cell> cells, int w, int h) throws CellCoordinateOutOfBoundariesException{
+	public Grid(ArrayList<Cell> cells, int w, int h) throws CellCoordinateOutOfBoundariesException {
 		setWidth(w);
 		setHeight(h);
 		grid = new Cell[width][height];
 		this.cells = cells;
-		for(int i = 0; i<cells.size(); i++){
-			if(cells.get(i).getX()>w || cells.get(i).getY()>h )
+		for (int i = 0; i < cells.size(); i++) {
+			if (cells.get(i).getX() > w || cells.get(i).getY() > h)
 				throw new CellCoordinateOutOfBoundariesException("");
 			else
 				grid[cells.get(i).getX()][cells.get(i).getY()] = cells.get(i);
 		}
 	}
-	
+
 	public Grid(int w, int h) throws CellCoordinateOutOfBoundariesException {
 		this(new ArrayList<Cell>(), w, h);
 	}
 
 	public String print() {
-	    return null;
+		return null;
 	}
-	
+
 	public Grid tick() throws NegativeCoordinateException {
 		ArrayList<Cell> tickedCells = new ArrayList<Cell>();
-		
-		for(int i = 0; i<4; i++){
-			for(int j = 0; j<4; j++){
-				if(grid[i][j].isAlive() && countAliveNeighbors(i, j)<2)
-					tickedCells.add(new Cell(i, j, false));	
-				else if(grid[i][j].isAlive() && (countAliveNeighbors(i, j)==2 || countAliveNeighbors(i, j)==3))
-					tickedCells.add(new Cell(i, j, true));
-				else if(grid[i][j].isAlive() && countAliveNeighbors(i, j)>3)
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (grid[i][j].isAlive() && countAliveNeighbors(i, j) < 2)
 					tickedCells.add(new Cell(i, j, false));
-				else if(!grid[i][j].isAlive() && countAliveNeighbors(i, j)==3)
+				else if (grid[i][j].isAlive() && (countAliveNeighbors(i, j) == 2 || countAliveNeighbors(i, j) == 3))
+					tickedCells.add(new Cell(i, j, true));
+				else if (grid[i][j].isAlive() && countAliveNeighbors(i, j) > 3)
+					tickedCells.add(new Cell(i, j, false));
+				else if (!grid[i][j].isAlive() && countAliveNeighbors(i, j) == 3)
 					tickedCells.add(new Cell(i, j, true));
 			}
 		}
-		
-	    return new Grid(tickedCells, width, height);
+
+		return new Grid(tickedCells, width, height);
 	}
-	
-	private int countAliveNeighbors(int x, int y){
+
+	private int countAliveNeighbors(int x, int y) {
 		int count = 0;
-		if (x==0 && y==0) {
-			//Upper-Left Angle
-			if (grid[x][y+1].isAlive())
+		if (x == 0 && y == 0) {
+			// Upper-Left Angle
+			if (grid[x][y + 1].isAlive())
 				count++;
-			if (grid[x+1][y].isAlive())
+			if (grid[x + 1][y].isAlive())
 				count++;
-			if (grid[x+1][y+1].isAlive())
+			if (grid[x + 1][y + 1].isAlive())
 				count++;
 		}
-		//Upper Side
-		if(grid[x][y-1].isAlive())
-			count++;
-		if(grid[x][y+1].isAlive())
-			count++;
-		if(grid[x+1][y-1].isAlive())
-			count++;
-		if(grid[x+1][y].isAlive())
-			count++;
-		if(grid[x+1][y+1].isAlive())
-			count++;
-		
+
+		else if (x == 0 && y != 0) {
+			// Upper Side
+			if (grid[x][y - 1].isAlive())
+				count++;
+			if (grid[x][y + 1].isAlive())
+				count++;
+			if (grid[x + 1][y - 1].isAlive())
+				count++;
+			if (grid[x + 1][y].isAlive())
+				count++;
+			if (grid[x + 1][y + 1].isAlive())
+				count++;
+		}
 		return count;
 	}
 
