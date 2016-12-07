26bff69e3b9b9f1b5594891e0299ca52669de212
somename <someemail>
2016-12-07 16:17:47 +0100

    besouro automatic message

--- src/tests/TestCell.java
+++ src/tests/TestCell.java
@@ -5,21 +5,19 @@ import org.junit.Test;
 
 import classes.Cell;
 import classes.NegativeCoordinateException;
-public class TestCell {
-	
-	Cell cella;
-	
-	@Test
-	public void firstCellTest(){
-		
-	}
-	
-	@Test(expected=NegativeCoordinateException.class)
-	public void testNegativeCoordinateForCell() throws NegativeCoordinateException{
-		cella=new Cell(-1,-1,true);
-	}
-	
-	
 
-	
+public class TestCell {
+
+	Cell cella;
+
+	@Test
+	public void firstCellTest() {
+
+	}
+
+	@Test(expected = NegativeCoordinateException.class)
+	public void testNegativeCoordinateForCell() throws NegativeCoordinateException {
+		cella = new Cell(-1, -1, true);
+	}
+
 }
