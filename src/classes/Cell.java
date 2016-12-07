4a39a28430d9cdf0eb41bb4e844b84c4b2b617ca
somename <someemail>
2016-12-07 15:14:38 +0100

    besouro automatic message

--- src/classes/Cell.java
+++ src/classes/Cell.java
@@ -8,4 +8,12 @@ public class Cell {
 	public Cell() throws NegativeCoordinateException {
 		
 	}
+	
+	public Cell(int x,int y,boolean t)throws NegativeCoordinateException{
+		if(x<0 && y<0) throw new NegativeCoordinateException("Coordinate negative");
+	
+	this.x=x;
+	this.y=y;
+	alive=t;
+	}
 }
