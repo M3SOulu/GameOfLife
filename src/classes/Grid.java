38de4033236b98035c6ccc32deec611f284ee983
somename <someemail>
2016-12-07 15:08:35 +0100

    besouro automatic message

--- src/classes/Grid.java
+++ src/classes/Grid.java
@@ -9,9 +9,13 @@ public class Grid {
 
 
 	public Grid(ArrayList<Cell> cells, int w, int h) throws CellCoordinateOutOfBoundariesException{
+		width=w;
+		height=h;
+		cells=new ArrayList<>(w*h);
 	}
 	
 	public Grid(int w, int h) throws CellCoordinateOutOfBoundariesException {
+		this(new ArrayList<Cell>(w*h),w,h);
 	}
 
 	public String print() {
