5bb20fa3ec9c0f0afda983f97aac3b233bee2e95
somename <someemail>
2016-12-07 15:20:52 +0100

    besouro automatic message

--- src/tests/TestCell.java
+++ src/tests/TestCell.java
@@ -1,8 +1,24 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 public class TestCell {
+	
+	@Before
+	public void setUp() throws Exception{
+		
+	}
+	
+	@After
+	public void TearDown() throws Exception{
+		
+		
+	}
+	
+	
 	@Test
 	public void firstCellTest(){
 		fail("implement some proper tests");
