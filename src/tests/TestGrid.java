d786d1f7835082f3aff68bf5ec4997c5a1242a73
somename <someemail>
2016-12-07 15:17:56 +0100

    besouro automatic message

--- src/tests/TestGrid.java
+++ src/tests/TestGrid.java
@@ -1,13 +1,33 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import classes.Cell;
+import classes.CellCoordinateOutOfBoundariesException;
+import classes.Grid;
+import classes.NegativeCoordinateException;
 
 
 public class TestGrid {
+	
+	Grid grid;
+	ArrayList<Cell> cells;
+	
+	@Before
+	public void setup(){
+		cells = new ArrayList<Cell>();
+	}
 
-	@Test
-	public void firstGridTest(){
-		fail("implement some proper tests");
+	@Test(expected = CellCoordinateOutOfBoundariesException.class)
+	public void ifACellIsOutOfTheGridLimitsAnExceptionShouldBeRised() throws CellCoordinateOutOfBoundariesException, NegativeCoordinateException{
+		//Arrange
+		Cell cell = new Cell(5,5, true);
+		//Act
+		grid = new Grid()
 	}
 }
