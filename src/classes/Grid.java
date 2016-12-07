ca6defebe9aa71fd37cd07a75ef4fecc4d26ae5b
somename <someemail>
2016-12-07 16:24:38 +0100

    besouro automatic message

--- src/classes/Grid.java
+++ src/classes/Grid.java
@@ -8,28 +8,6 @@ public class Grid {
 	private int width;
 	private int height;
 
-
-	public Grid(ArrayList<Cell> cells, int w, int h) throws CellCoordinateOutOfBoundariesException{
-	}
-	
-	public Grid(int w, int h) throws CellCoordinateOutOfBoundariesException {
-	}
-
-	public String print() {
-		
-		String state = null;
-		ArrayList<Cell> cell = null;
-		Grid grid = new Grid(cell,10,10);
-		//if(grid.getHeight() != grid.getWidth()) state = "Grid invalid";	
-		Iterator<Cell> i;
-		return state;   
-			
-	}
-	
-	public Grid tick() {
-	    return null;
-	}
-
 	public ArrayList<Cell> getCells() {
 		return cells;
 	}
@@ -53,4 +31,31 @@ public class Grid {
 	public void setHeight(int height) {
 		this.height = height;
 	}
+
+	public Grid(ArrayList<Cell> cells, int w, int h) throws CellCoordinateOutOfBoundariesException{
+	}
+	
+	public Grid(int w, int h) throws CellCoordinateOutOfBoundariesException {
+	}
+
+	public String print() {
+		
+		String state = null;
+		int i;
+		ArrayList<Cell> cell = new ArrayList<Cell>();
+		Grid grid = new Grid(cell,10,10);
+		//if(grid.getHeight() != grid.getWidth()) state = "Grid invalid";	
+	
+		return state;   
+		for(i = 0; i < cell.size();i++) {
+			if(cell.ge)
+		}
+			
+	}
+	
+	public Grid tick() {
+	    return null;
+	}
+
+
 }
