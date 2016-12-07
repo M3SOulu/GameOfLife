3954489e6a95b1dd475f63d92cfcf5aa766849f1
somename <someemail>
2016-12-07 16:30:33 +0100

    besouro automatic message

--- src/classes/Grid.java
+++ src/classes/Grid.java
@@ -7,46 +7,57 @@ public class Grid {
 	private int width;
 	private int height;
 
+	public Grid(ArrayList<Cell> cells, int w, int h)
+			throws CellCoordinateOutOfBoundariesException, CellCoordinateDuplicate {
+		width = w;
+		height = h;
+		this.cells = new ArrayList<Cell>();
+		for (Cell cell : cells) {
+			if (cell.getX() > height || cell.getY() > width)
+				throw new CellCoordinateOutOfBoundariesException("CellCoordinateOutOfBoundaries");
 
-	public Grid(ArrayList<Cell> cells, int w, int h) throws CellCoordinateOutOfBoundariesException, CellCoordinateDuplicate{
-		width=w;
-		height=h;
-		this.cells=new ArrayList<Cell>();
-		for(Cell cell : cells){
-			if(cell.getX()>height || cell.getY()>width)
-					throw new CellCoordinateOutOfBoundariesException("CellCoordinateOutOfBoundaries");
-			
-			for(Cell newCell: this.cells){
-				if(newCell.getX()==cell.getX() && newCell.getY()==cell.getY())
+			for (Cell newCell : this.cells) {
+				if (newCell.getX() == cell.getX() && newCell.getY() == cell.getY())
 					throw new CellCoordinateDuplicate("CellCordinateDuplicate");
 			}
-			
+
 			this.cells.add(cell);
 		}
-		
+
 	}
-	
+
 	public Grid(int w, int h) throws CellCoordinateOutOfBoundariesException {
-		width=w;
-		height=h;
-		
+		width = w;
+		height = h;
+
+	}
+
+	private boolean controlCellsConformity(ArrayList<Cell> arrayCells) {
+		int alive = 0;
+		int dead = 0;
+
+		for (Cell cell : arrayCells) {
+			if (cell.getState().equals("alive"))
+				alive++;
+		}
+
+		return false;
 	}
 
 	public String print() {
-	    return null;
+		return null;
 	}
-	
+
 	public Grid tick() {
-	    return null;
+		return null;
 	}
-	
-	public int getWidth(){
+
+	public int getWidth() {
 		return width;
 	}
-	
-	public int getHeight(){
+
+	public int getHeight() {
 		return height;
 	}
-	
-	
+
 }
