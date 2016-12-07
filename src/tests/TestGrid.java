c96758eec32f22480db6d61461e495714c034b9a
somename <someemail>
2016-12-07 15:24:36 +0100

    besouro automatic message

--- src/tests/TestGrid.java
+++ src/tests/TestGrid.java
@@ -3,11 +3,20 @@ package tests;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import classes.CellCoordinateOutOfBoundariesException;
+import classes.Grid;
+
 
 public class TestGrid {
+	Grid griglia;
 
 	@Test
 	public void firstGridTest(){
 		fail("implement some proper tests");
+	}
+	
+	@Test(expected=CellCoordinateOutOfBoundariesException.class)
+	public void grigliaErrataException(){
+		griglia=new Grid(50,10);
 	}
 }
