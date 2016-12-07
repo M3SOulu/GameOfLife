09b509644110ff947d6a245941f9bf7372708980
somename <someemail>
2016-12-07 15:31:39 +0100

    besouro automatic message

--- src/tests/TestGrid.java
+++ src/tests/TestGrid.java
@@ -1,14 +1,36 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import classes.Cell;
+import classes.Grid;
 
 
 public class TestGrid {
 	
+	private Grid grid;
+	private ArrayList<Cell> cell;
+	private int x;
+	private int y;
 
+	@Before
+	public void inizialize() {
+		grid = new Grid(x,y);
+		
+	}
+	
 	@Test
 	public void firstGridTest(){
+		fail("implement some proper tests");
+	}
+	
+	@Test
+	public void GridCoordinateOutOfBound(){
 		fail("implement some proper tests");
 	}
 }
