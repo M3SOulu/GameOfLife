2e6f4d836d3b382f66b19384092783bc314f13b9
somename <someemail>
2016-12-07 15:31:38 +0100

    besouro automatic message

--- src/tests/TestGrid.java
+++ src/tests/TestGrid.java
@@ -1,11 +1,28 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import classes.Grid;
 
 
 public class TestGrid {
 
+	Grid grid;
+	
+	@Before
+	public void initialize(){
+		grid = new Grid(3,3);
+	}
+	
+	@Test
+	public void testGridSize(){
+
+		assertEqual(grid.getWidth,grid.getHeight);
+	}
+	
 	@Test
 	public void firstGridTest(){
 		fail("implement some proper tests");
