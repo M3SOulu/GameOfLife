98a1b2ef4b660fccdb9f07288558060ad4ab453a
somename <someemail>
2016-12-07 15:50:11 +0100

    besouro automatic message

--- src/tests/TestGrid.java
+++ src/tests/TestGrid.java
@@ -19,7 +19,7 @@ public class TestGrid {
 		assertEquals(10, grid.getWidth());
 		assertEquals(10, grid.getHeight());
 	}
-	
+
 	@Test
 	public void eachCellShallBeAccessibleByUniqueXY() throws NegativeCoordinateException{
 		//Arrange
@@ -28,16 +28,16 @@ public class TestGrid {
 		cells.add(new Cell(true, 1, 2));
 		cells.add(new Cell(true, 2, 1));
 		cells.add(new Cell(true, 2, 2));
-		
+
 		Grid grid = new Grid(cells, 2, 2);
 		//Act
 		Cell cell = grid.get(2, 1);
-		
+
 		//Assert
 		assertEquals(2, cell.getX());
 		assertEquals(1, cell.getY());
 	}
-	
+
 	@Test
 	public void numberOfAliveCellsShouldBeInitializedRoughlyEqualToDeadCellNumber() 
 			throws NegativeCoordinateException{
@@ -54,7 +54,28 @@ public class TestGrid {
 		cells.add(new Cell(true, 3, 3));
 		//Act
 		Grid grid = new Grid(cells, 3, 3);
-		
+
+		//Assert
+		assertEquals(5, grid.getAliveCells());
+		assertEquals(4, grid.getDeadCells());
+	}
+
+	@Test
+	public void getNeighborsCellInsideTheGrid() throws NegativeCoordinateException{
+		//Arrange
+		ArrayList<Cell> cells = new ArrayList<>();
+		cells.add(new Cell(true, 1, 1));
+		cells.add(new Cell(false, 1, 2));
+		cells.add(new Cell(true, 1, 3));
+		cells.add(new Cell(false, 2, 1));
+		cells.add(new Cell(true, 2, 2));
+		cells.add(new Cell(false, 2, 3));
+		cells.add(new Cell(true, 3, 1));
+		cells.add(new Cell(false, 3, 2));
+		cells.add(new Cell(true, 3, 3));
+		//Act
+		Grid grid = new Grid(cells, 3, 3);
+
 		//Assert
 		assertEquals(5, grid.getAliveCells());
 		assertEquals(4, grid.getDeadCells());
