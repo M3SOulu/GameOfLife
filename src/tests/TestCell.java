3a0fde6c73d8823cea705b5d01dce97ac084ab72
somename <someemail>
2016-12-07 15:24:33 +0100

    besouro automatic message

--- src/tests/TestCell.java
+++ src/tests/TestCell.java
@@ -1,27 +1,38 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import classes.Cell;
+import classes.NegativeCoordinateException;
+
 public class TestCell {
-	
+
 	Cell cell;
-	
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		cell = new Cell();
 	}
-	
+
 	@Test
-	public void firstCellTest(){
+	public void firstCellTest() {
 		boolean isAlive = false;
 		isAlive = cell.getState();
-		
-		if(isAlive == true){
-			assertTrue(isAlive,true);
-		}else{
-			assertTrue(isAlive,false);
+
+		if (isAlive == true) {
+			assertTrue(isAlive);
+		} else {
+			assertFalse(isAlive);
 		}
 	}
+	
+	@Test (expected = NegativeCoordinateException.class)
+	public void negativeCoordinateOfCell() throws NegativeCoordinateException{
+		cell = new Cell(1,-3);
+	}
+
 }
