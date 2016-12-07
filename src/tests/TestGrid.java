eb4eded31699f0d0e18b270d3ee86d6803734d75
somename <someemail>
2016-12-07 15:46:05 +0100

    besouro automatic message

--- src/tests/TestGrid.java
+++ src/tests/TestGrid.java
@@ -1,13 +1,23 @@
 package tests;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import classes.CustomLifeException;
+import classes.Grid;
 
 
 public class TestGrid {
 
-	@Test
-	public void firstGridTest(){
-		fail("implement some proper tests");
+	@Test (expected = CustomLifeException)
+	public void isGridNotASquareArrengment(){
+		try {
+			Grid grid = new Grid(20, 20);
+		}
+		catch (CustomLifeException e) {
+			// to do
+		}
+		
+		
+		
 	}
 }
