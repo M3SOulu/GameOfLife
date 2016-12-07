2e524833948b65155d6e1a7d520091d826643011
somename <someemail>
2016-12-07 15:24:30 +0100

    besouro automatic message

--- src/tests/TestGrid.java
+++ src/tests/TestGrid.java
@@ -1,8 +1,12 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import classes.Cell;
 import classes.Grid;
 
 
@@ -15,5 +19,14 @@ public class TestGrid {
 		assertEquals(10, grid.getHeight());
 	}
 	
-	
+	@Test
+	public void eachCellShallBeAccessibleByUniqueXY(){
+		ArrayList<Cell> cells = new ArrayList<>();
+		cells.add(new Cell(true, 1, 1));
+		cells.add(new Cell(true, 1, 2));
+		cells.add(new Cell(true, 2, 1));
+		cells.add(new Cell(true, 2, 2));
+		
+		Grid grid = new Grid(cells, 2, 2);
+	}
 }
