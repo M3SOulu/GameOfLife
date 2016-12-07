651f9683d77d7939a4391c98fdee1bd05401a1dd
somename <someemail>
2016-12-07 15:41:31 +0100

    besouro automatic message

--- src/tests/TestGrid.java
+++ src/tests/TestGrid.java
@@ -31,4 +31,29 @@ public class TestGrid {
 		//Act
 		grid = new Grid(cells, 4, 4);
 	}
+	
+	@Test 
+	public void trickingALiveCellWithLessThan2LiveNeighborsMakesItDie() throws NegativeCoordinateException{
+		//Arrange
+		Cell liveCell = new Cell(5,5, true);
+		Cell deadNeighbor1 = new Cell(4,5, false);
+		Cell deadNeighbor2 = new Cell(6,5, false);
+		Cell deadNeighbor3 = new Cell(5,4, false);
+		Cell deadNeighbor4 = new Cell(5,6, false);
+		Cell deadNeighbor5 = new Cell(4,4, false);
+		Cell deadNeighbor6 = new Cell(6,4, false);
+		Cell deadNeighbor7 = new Cell(4,6, false);
+		Cell deadNeighbor8 = new Cell(6,6, false);
+		cells.add(liveCell);
+		cells.add(deadNeighbor1);
+		cells.add(deadNeighbor2);
+		cells.add(deadNeighbor3);
+		cells.add(deadNeighbor4);
+		cells.add(deadNeighbor5);
+		cells.add(deadNeighbor6);
+		cells.add(deadNeighbor7);
+		cells.add(deadNeighbor8);
+		
+		
+	}
 }
