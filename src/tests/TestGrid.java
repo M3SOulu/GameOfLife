7b821cd716b318384899182537f142d138ada84d
somename <someemail>
2016-12-07 16:32:24 +0100

    besouro automatic message

--- src/tests/TestGrid.java
+++ src/tests/TestGrid.java
@@ -12,43 +12,43 @@ import classes.CellCoordinateOutOfBoundariesException;
 import classes.Grid;
 import classes.NegativeCoordinateException;
 
-
 public class TestGrid {
-	
+
 	Grid grid;
 	ArrayList<Cell> cells;
-	
+
 	@Before
-	public void setup(){
+	public void setup() {
 		cells = new ArrayList<Cell>();
 	}
 
 	@Test(expected = CellCoordinateOutOfBoundariesException.class)
-	public void ifACellIsOutOfTheGridLimitsAnExceptionShouldBeRised() throws CellCoordinateOutOfBoundariesException, NegativeCoordinateException{
-		//Arrange
-		Cell cell = new Cell(5,5, true);
+	public void ifACellIsOutOfTheGridLimitsAnExceptionShouldBeRised()
+			throws CellCoordinateOutOfBoundariesException, NegativeCoordinateException {
+		// Arrange
+		Cell cell = new Cell(5, 5, true);
 		cells.add(cell);
-		//Act
+		// Act
 		grid = new Grid(cells, 4, 4);
 	}
-	
-	@Test 
-	public void trickingAnAngleAliveCellWithLessThan2LiveNeighborsMakesItDie() throws NegativeCoordinateException{
-		//Arrange
-		Cell liveCell = new Cell(0,0, true);
-		Cell liveNeighbor1 = new Cell(0,1, true);
-		Cell deadNeighbor2 = new Cell(1,0, false);
-		Cell deadNeighbor3 = new Cell(1,1, false);
-		
+
+	@Test
+	public void trickingAnAngleAliveCellWithLessThan2LiveNeighborsMakesItDie() throws NegativeCoordinateException {
+		// Arrange
+		Cell liveCell = new Cell(0, 0, true);
+		Cell liveNeighbor1 = new Cell(0, 1, true);
+		Cell deadNeighbor2 = new Cell(1, 0, false);
+		Cell deadNeighbor3 = new Cell(1, 1, false);
+
 		cells.add(liveCell);
 		cells.add(liveNeighbor1);
 		cells.add(deadNeighbor2);
 		cells.add(deadNeighbor3);
 
 		grid = new Grid(cells, 3, 3);
-		//Act
+		// Act
 		grid = grid.tick();
-		//Assert
-		assertTrue(!grid.getCells().get(0).isAlive());		
+		// Assert
+		assertTrue(!grid.getCells().get(0).isAlive());
 	}
 }
