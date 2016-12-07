79f298e785c6e7ed6f268e1aadc46e954117ccba
somename <someemail>
2016-12-07 15:21:31 +0100

    besouro automatic message

--- src/tests/TestCell.java
+++ src/tests/TestCell.java
@@ -1,9 +1,21 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import classes.Cell;
+import classes.NegativeCoordinateException;
+
 public class TestCell {
+	
+	private Cell cell;
+	
+	@Before
+	public void inizialize() throws NegativeCoordinateException{
+		cell = new Cell(10,10,true);
+	}
 	@Test
 	public void firstCellTest(){
 		fail("implement some proper tests");
