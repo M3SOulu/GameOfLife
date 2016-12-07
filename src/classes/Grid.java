158965eab177f4db8ffc7cc2bb6187593e0b5fad
studente <studente@turing-10.boole.local>
2016-12-07 17:17:41 +0100

    finish

--- src/classes/Grid.java
+++ src/classes/Grid.java
@@ -8,10 +8,35 @@ public class Grid {
 	private int height;
 
 
-	public Grid(ArrayList<Cell> cells, int w, int h) throws CellCoordinateOutOfBoundariesException{
+	public Grid(ArrayList<Cell> cells, int w, int h) throws CellCoordinateOutOfBoundariesException, CustomLifeException{
+		if( w != h){
+			throw new CustomLifeException("Error: the grid is square");
+		}
+		
+		this.width = w;
+		this.height = h;
+		
+		
+
+		
 	}
 	
-	public Grid(int w, int h) throws CellCoordinateOutOfBoundariesException {
+	public Grid(int w, int h) throws CellCoordinateOutOfBoundariesException, CustomLifeException {
+		if( w != h){
+			throw new CustomLifeException("Error: the grid is square");
+		}
+		
+		this.width = w;
+		this.height = h;
+		
+	}
+	
+	public int getWidth(){
+		return this.width;
+	}
+	
+	public int getHeight(){
+		return this.height;
 	}
 
 	public String print() {
--- src/tests/TestCell.java
+++ src/tests/TestCell.java
@@ -1,10 +1,62 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import classes.Cell;
+import classes.NegativeCoordinateException;
 public class TestCell {
-	@Test
-	public void firstCellTest(){
-		fail("implement some proper tests");
+	
+	Cell cell;
+	
+	@Before
+	public void setUp() throws NegativeCoordinateException{
+		cell = new Cell();
 	}
+	
+	
+	
+	@Test
+	public void statusCellTest(){
+		//Act
+		String status = cell.status();
+		//Assert
+		assertEquals("alive", status);
+	}
+	
+	@Test
+	public void changeStatusCellTest(){
+		//Act
+		cell.changeStatus();
+		String status = cell.status();
+		//Assert
+		assertEquals("dead", status);
+	}
+	
+	@Test
+	public void coordinateCellTest() throws NegativeCoordinateException{
+		//Act
+		cell.setXY(10);
+		int x = cell.getX();
+		int y = cell.getY();
+		//Assert
+		assertEquals(10, x);
+		assertEquals(10, y);
+	}
+	
+	@Test(expected = NegativeCoordinateException.class)
+	public void negativeCoordinateCellTest() throws NegativeCoordinateException{
+		//Act
+		cell.setXY(-1);
+		int x = cell.getX();
+		int y = cell.getY();
+		//Assert
+		assertEquals(-1, x);
+		assertEquals(-1, y);
+	}
+	
+	
+	
 }
