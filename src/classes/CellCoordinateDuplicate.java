c5fcdb0f44e55d976e7188ae8fe38949e5733294
somename <someemail>
2016-12-07 16:10:17 +0100

    besouro automatic message

--- src/classes/CellCoordinateDuplicate.java
+++ src/classes/CellCoordinateDuplicate.java
@@ -2,6 +2,11 @@ package classes;
 
 public class CellCoordinateDuplicate extends Exception{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	CellCoordinateDuplicate(String message){
 		super(message);
 	}
