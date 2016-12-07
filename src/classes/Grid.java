96c73f22dbce6160b58b367bead97b7a0551fec4
somename <someemail>
2016-12-07 16:04:15 +0100

    besouro automatic message

--- src/classes/Grid.java
+++ src/classes/Grid.java
@@ -11,10 +11,10 @@ public class Grid {
 	public Grid(ArrayList<Cell> cells, int w, int h) throws CellCoordinateOutOfBoundariesException{
 		width=w;
 		height=h;
-		cells=new ArrayList<Cell>();
+		this.cells=new ArrayList<Cell>();
 		for(Cell cell : cells){
 			System.out.println(cell.getX()+","+cell.getY());
-			if(cell.getX()>height || cell.getX()>width)
+			if(cell.getX()>height || cell.getY()>width)
 					throw new CellCoordinateOutOfBoundariesException("CellCoordinateOutOfBoundaries");
 			
 			this.cells.add(cell);
@@ -44,8 +44,16 @@ public class Grid {
 		return height;
 	}
 	
-	public static void main(String args[]){
+	public static void main(String args[]) throws NegativeCoordinateException{
+		System.out.println("gsgs");
+		Grid grid;
+		ArrayList<Cell> arrayCell;
+		arrayCell = new ArrayList<Cell>();
+		arrayCell.add(new Cell(1,1));
+		arrayCell.add(new Cell(1,2));
+		arrayCell.add(new Cell(1,4));
 		grid = new Grid(arrayCell,3,3);
+		
 	}
 	
 }
