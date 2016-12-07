a1a988e515293f872f7a241d3f8d3df5b8e98ba9
somename <someemail>
2016-12-07 15:23:23 +0100

    besouro automatic message

--- src/classes/Grid.java
+++ src/classes/Grid.java
@@ -12,9 +12,12 @@ public class Grid {
 		for(int i = 0; i<cells.size(); i++)
 			if(cells.get(i).getX()>w || cells.get(i).getY()>h )
 				throw new CellCoordinateOutOfBoundariesException("");
+		width = w;
+		height = h;
 	}
 	
 	public Grid(int w, int h) throws CellCoordinateOutOfBoundariesException {
+		this(new ArrayList<Cell>(), w, h);
 	}
 
 	public String print() {
