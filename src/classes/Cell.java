04bfaca867b487ca84817ab41993278665f0936d
somename <someemail>
2016-12-07 15:04:43 +0100

    besouro automatic message

--- src/classes/Cell.java
+++ src/classes/Cell.java
@@ -4,6 +4,19 @@ public class Cell {
 	private int x;
 	private int y;
 	private boolean alive;
+	
+	
 	public Cell() throws NegativeCoordinateException {
 	}
+	
+	
+	
+	public void makeAlive() {
+		this.alive = true;
+	}
+	
+	public void makeDead() {
+		this.alive = false;
+	}
+	
 }
